50cd0cd59b74ec0eb59a4b47aa0078192296f7e4
nosorae <nosorae@naver.com>
2022-02-20 22:41:20 +0900

    Chore: 쓰레기 코드 제거

--- app/src/main/java/com/nosorae/thingsflow/common/Constants.kt
+++ app/src/main/java/com/nosorae/thingsflow/common/Constants.kt
@@ -8,8 +8,6 @@ object Constants {
 
     const val PARAM_ISSUE_MODEL = "param_issue_model"
 
-    const val LOG_TAG = "log_tag"
-
     const val DATABASE_NAME = "issue_db"
 
     const val PREF_NAME = "pref_name"
